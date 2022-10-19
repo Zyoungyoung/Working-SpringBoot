d6ca42441595e8659d6d27d9133080590883c4a0
zyy <2930194661@qq.com>
2022-10-19 16:48:34 +0800

    进销存系统

---
+++ src/main/java/com/zyy/InstorymgApplication.java
@@ -0,0 +1,13 @@
+package com.zyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstorymgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstorymgApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/zyy/controller/Hellow.java
@@ -0,0 +1,16 @@
+package com.zyy.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hellow {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+
+}
